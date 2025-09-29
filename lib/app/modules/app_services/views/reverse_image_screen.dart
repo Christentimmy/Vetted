32632c9c5d8b45d7738ef094fa199926a93d0574
Christentimmy <timmychris09@gmail.com>
2025-09-29 18:15:58 +0300

    reverse image

--- lib/app/modules/app_services/views/reverse_image_screen.dart
+++ lib/app/modules/app_services/views/reverse_image_screen.dart
@@ -2,6 +2,7 @@ import 'package:Vetted/app/controller/app_service_controller.dart';
 import 'package:Vetted/app/data/models/search_image_model.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class ReverseImageScreen extends StatefulWidget {
   const ReverseImageScreen({super.key});
@@ -23,7 +24,6 @@ class _ReverseImageScreenState extends State<ReverseImageScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-
       appBar: AppBar(
         title: const Text("Reverse Image Search"),
         centerTitle: true,
@@ -43,21 +43,12 @@ class _ReverseImageScreenState extends State<ReverseImageScreen> {
           gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
             crossAxisCount: 2,
             crossAxisSpacing: 10,
+            mainAxisSpacing: 20,
           ),
           itemBuilder: (context, index) {
             final image = appServiceController.images[index];
 
-            return InkWell(
-              onTap: () {
-                showDialog(
-                  context: context,
-                  builder: (context) {
-                    return Dialog(child: Image.network(image.imageUrl));
-                  },
-                );
-              },
-              child: _MatchTile(searchImage: image),
-            );
+            return _MatchTile(searchImage: image);
           },
         ),
       ),
@@ -70,6 +61,24 @@ class _MatchTile extends StatelessWidget {
 
   const _MatchTile({required this.searchImage});
 
+  Future<void> urlLauncher(String url) async {
+    final Uri uri = Uri.parse(url);
+
+    // Encode query parameters to avoid issues with '?' or special characters
+    final Uri encodedUri = Uri(
+      scheme: uri.scheme,
+      host: uri.host,
+      path: uri.path,
+      query: uri.query,
+    );
+
+    try {
+      await launchUrl(encodedUri, mode: LaunchMode.externalApplication);
+    } catch (e) {
+      debugPrint(e.toString());
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return ClipRRect(
@@ -77,22 +86,37 @@ class _MatchTile extends StatelessWidget {
       child: Stack(
         alignment: Alignment.bottomCenter,
         children: [
-          Image.network(
-            searchImage.imageUrl,
-            fit: BoxFit.cover,
-            width: double.infinity,
-            height: double.infinity,
+          InkWell(
+            onTap: () {
+              showDialog(
+                context: context,
+                builder: (context) {
+                  return Dialog(child: Image.network(searchImage.imageUrl));
+                },
+              );
+            },
+            child: Image.network(
+              searchImage.imageUrl,
+              fit: BoxFit.cover,
+              width: double.infinity,
+              height: double.infinity,
+            ),
           ),
-          Container(
-            width: double.infinity,
-            padding: const EdgeInsets.symmetric(vertical: 6),
-            color: Colors.white,
-            child: Text(
-              searchImage.source,
-              textAlign: TextAlign.center,
-              style: const TextStyle(
-                color: Colors.red,
-                fontWeight: FontWeight.w600,
+          InkWell(
+            onTap: () async {
+              await urlLauncher(searchImage.link);
+            },
+            child: Container(
+              width: double.infinity,
+              padding: const EdgeInsets.symmetric(vertical: 6),
+              color: Colors.white,
+              child: Text(
+                searchImage.source,
+                textAlign: TextAlign.center,
+                style: const TextStyle(
+                  color: Colors.red,
+                  fontWeight: FontWeight.w600,
+                ),
               ),
             ),
           ),
